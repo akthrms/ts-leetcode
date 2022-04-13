e9bddb6ae0e3ed22d7e5e969b7f50875f7a38147
akthrms <akt.hrms@gmail.com>
2022-04-13 22:42:10 +0900

    update

---
+++ src/101.symmetric-tree.ts
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=101 lang=typescript
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isSymmetric(root: TreeNode | null): boolean {
+  if (root === null) {
+    return true;
+  } else {
+    return isMirror(root.left, root.right);
+  }
+}
+
+const isMirror = (left: TreeNode | null, right: TreeNode | null): boolean => {
+  if (left !== null && right !== null) {
+    return (
+      left.val === right.val &&
+      isMirror(left.left, right.right) &&
+      isMirror(left.right, right.left)
+    );
+  } else {
+    return left == right;
+  }
+};
+// @lc code=end
---
+++ src/94.binary-tree-inorder-traversal.ts
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=94 lang=typescript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  let stack = [];
+  let result = [];
+  let current = root;
+
+  while (true) {
+    if (current !== null) {
+      stack.push(current);
+      current = current.left;
+      continue;
+    }
+
+    if (stack.length > 0) {
+      current = stack.pop()!;
+      result.push(current.val);
+      current = current.right;
+      continue;
+    }
+
+    break;
+  }
+
+  return result;
+}
+// @lc code=end
