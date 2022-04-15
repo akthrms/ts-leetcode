6350259b7a379d3f46f38d8c4b2cd20607db9319
akthrms <akt.hrms@gmail.com>
2022-04-15 23:53:32 +0900

    update

---
+++ src/234.palindrome-linked-list.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=234 lang=typescript
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  let list = [];
+  let current = head;
+
+  while (current !== null) {
+    list.push(current.val);
+    current = current.next;
+  }
+
+  return list.toString() == list.reverse().toString();
+}
+// @lc code=end
