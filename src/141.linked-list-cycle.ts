f221bd649daa1c6abe346319fa57821a2f379803
akthrms <akt.hrms@gmail.com>
2022-04-15 20:08:30 +0900

    update

---
+++ src/141.linked-list-cycle.ts
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=141 lang=typescript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  if (head === null) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (slow !== null && fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+// @lc code=end
