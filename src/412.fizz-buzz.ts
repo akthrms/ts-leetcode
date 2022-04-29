f144b1ad284a31308bf7ae32c64a4ca5689284fc
akthrms <akt.hrms@gmail.com>
2022-04-30 02:46:29 +0900

    update

---
+++ src/412.fizz-buzz.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=412 lang=typescript
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+function fizzBuzz(n: number): string[] {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(i.toString());
+    }
+  }
+
+  return result;
+}
+// @lc code=end
